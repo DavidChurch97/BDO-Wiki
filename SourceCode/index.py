99618e32dcba669d404be6112d9be9e08eb41011
David Church <Davidchurch07@gmail.com>
2017-11-09 14:31:45 +0000

    Fixed index.html as the header image was not loading

--- SourceCode/index.py
+++ SourceCode/index.py
@@ -1,10 +1,13 @@
 import ConfigParser
+import logging
+
+from logging.handlers import RotatingFileHandler
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def Home():
-	return render_template("index.html"), 200
+    return render_template("index.html"), 200
 
 @app.route("/guides")
 def Guides():
